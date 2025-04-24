11a1f77009b3edb349d236a340c6f14ba9c52e45
Maximiliano Olivera <angelotunado02@gmail.com>
2025-04-24 15:04:24 -0300

    feat(version): fix version and payment logic"

---
+++ hooks/useInAppPurchease.tsx
@@ -0,0 +1,65 @@
+import { useToastContext } from "@/contexts/ToastContext";
+import api from "@/services/api/admin";
+import { useToast } from "native-base";
+import React, { useEffect, useState } from "react";
+import {
+  purchaseUpdatedListener,
+  finishTransaction,
+  Purchase,
+  ProductPurchase,
+} from "react-native-iap";
+import { useUser } from "./redux/useUser";
+
+export function useIAPHandler(empresaId: any) {
+  const { showToast } = useToastContext();
+  const { handlePayOk } = useUser();
+
+  useEffect(() => {
+    const purchaseListener = purchaseUpdatedListener(
+      async (purchase: ProductPurchase) => {
+        console.log("purchaseUpdatedListener ejecutado")
+        const { purchaseToken } = purchase;
+
+        try {
+          const res = await api.payments.verifyPaymentIsOk({
+            empresaId: empresaId ?? "",
+            purcheaseToken: purchaseToken ?? "",
+          });
+
+          console.log("xd1 res", res)
+          if (res && res?.success) {
+            showToast({
+              title: "Suscripcion realizada exitosamente",
+              descripcion: "",
+              status: "success",
+            });
+            console.log("finishTransaction", purchase)
+            handlePayOk();
+            await finishTransaction(purchase as any);
+          } else {
+            console.log('xd1', res?.data)
+            showToast({
+              title: "No se pudo verificar la suscripción'",
+              descripcion: "Contacta con soporte",
+              status: "error",
+            });
+          }
+          await finishTransaction(purchase as any);
+        } catch (e) {
+          console.error("Error al verificar compra con el backend:", e);
+          showToast({
+            title: "No se pudo verificar la suscripción'",
+            descripcion: "Contacta con soporte",
+            status: "error",
+          });
+        }
+      }
+    );
+
+    return () => {
+      if (purchaseListener) {
+        purchaseListener.remove();
+      }
+    };
+  }, [empresaId]);
+}
