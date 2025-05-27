eaabc0a32bc4d88d6212ead85ce413999f8e5673
JorgeMRs <jocady21@gmail.com>
2025-05-27 20:22:52 +0000

    May 27, 2025, 5:22 PM

---
+++ hooks/PDF/PDFGenerate.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import RNPrint from "react-native-print";
+
+export const useThermalPrint = () => {
+  const [loading, setLoading] = useState(false);
+
+  const printHTML = async (htmlContent: string) => {
+    setLoading(true);
+    try {
+      await RNPrint.print({
+        html: htmlContent,
+      });
+
+      return true;
+    } catch (error) {
+      console.log("Error al imprimir:", error);
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { printHTML, loading };
+};
