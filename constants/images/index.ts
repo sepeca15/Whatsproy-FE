b96aa670684b39319b3de75e2ec677332eb96a1d
JorgeMRs <jocady21@gmail.com>
2025-06-20 18:16:46 +0000

    Jun 20, 2025, 3:16 PM

--- constants/images/index.ts
+++ constants/images/index.ts
@@ -2,7 +2,7 @@
 export const GuideImages = {
     dashboard: {
         main: require("./Panel_principal.jpeg"),
-        metrics: require("../logo.jpeg"),
+        metrics: require("./Pedidos3.jpeg"),
         orders: require("../logo.jpeg"),
     },
     schedule: {
